a232a31b365a6f42ffe86ffdff04662177f296bf
Jakob Povšič <jakob.povsic@gmail.com>
2022-07-23 12:30:23 -0400

    fix: deposit amounts

--- src/setup.ts
+++ src/setup.ts
@@ -3,7 +3,6 @@ require("dotenv").config();
 import {
   MarginfiClient,
   MarginfiAccount,
-  uiToNative,
 } from "@mrgnlabs/marginfi-client";
 import { POSITION_SIZE_USD } from "./bot";
 
@@ -16,42 +15,56 @@ import { POSITION_SIZE_USD } from "./bot";
 
   // Get marginfi account from .env config.
   const mfiClient = await MarginfiClient.fromEnv();
-  const mfiAccount = await tryOrCry(mfiClient.createMarginfiAccount(), "Creating marginfi account");
+  const mfiAccount = await tryOrCry(
+    mfiClient.createMarginfiAccount(),
+    "Creating marginfi account"
+  );
 
   console.log("Account address %s", mfiAccount.publicKey);
-  
-  await tryOrCry(mfiAccount.deposit(uiToNative(POSITION_SIZE_USD)), "Depositing into marginfi account");
+
+  await tryOrCry(
+    mfiAccount.deposit(POSITION_SIZE_USD),
+    "Depositing into marginfi account"
+  );
 
   // Setup UTPs.
   await setupZo(mfiAccount);
   await setupMango(mfiAccount);
 
   console.log("Done. Add MARGINFI_ACCOUNT=%s to .env", mfiAccount.publicKey);
-})()
+})();
 
 async function setupZo(mfiAccount: MarginfiAccount) {
   console.log("Setting up 01");
-  const zo = mfiAccount.zo
+  const zo = mfiAccount.zo;
 
   await tryOrCry(zo.activate(), "Activating 01 Protocol");
-  await tryOrCry(zo.deposit(uiToNative(POSITION_SIZE_USD / 2, 6)), "Depositing into 01 Protocol");
+  await tryOrCry(
+    zo.deposit(POSITION_SIZE_USD / 2),
+    "Depositing into 01 Protocol"
+  );
 }
 
 async function setupMango(mfiAccount: MarginfiAccount) {
   console.log("Setting up Mango");
-  const mango = mfiAccount.mango
+  const mango = mfiAccount.mango;
 
   await tryOrCry(mango.activate(), "Activating Mango Protocol");
-  await tryOrCry(mango.deposit(uiToNative(POSITION_SIZE_USD / 2, 6)), "Depositing into Mango Protocol");
+  await tryOrCry(
+    mango.deposit(POSITION_SIZE_USD / 2),
+    "Depositing into Mango Protocol"
+  );
 }
 
-
-async function tryOrCry<G>(promise: Promise<G>, actionText: string): Promise<G> {
+async function tryOrCry<G>(
+  promise: Promise<G>,
+  actionText: string
+): Promise<G> {
   console.log("%s", actionText.toUpperCase());
   try {
-    return await promise
+    return await promise;
   } catch (e) {
     console.log("%s FAILED", actionText.toUpperCase());
-    throw e
+    throw e;
   }
 }
